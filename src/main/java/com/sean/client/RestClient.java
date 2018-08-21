7d4ceb0d106fa8e91a728d38a8eb4a25c9fbcf38
Sean <shengsheng.ss@alibaba-inc.com>
2018-08-21 11:27:32 +0800

    first commit

---
+++ src/main/java/com/sean/client/RestClient.java
@@ -0,0 +1,13 @@
+package com.sean.client;
+
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author sean
+ * @version Id:,v0.1 2018/8/20 下午5:59 sean Exp $
+ * @description
+ */
+public class RestClient {
+
+    private RestTemplate restTemplate;
+}
